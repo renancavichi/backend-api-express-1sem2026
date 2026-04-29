f7d9edf7cc59873419a98af6e68daad760a64fbe
Renan Cavichi <renancavichi@gmail.com>
2026-04-28 22:10:42 -0300

    add validation to deleteUsersController and enhance user schema error messages

--- src/controllers/user/deleteUserController.js
+++ src/controllers/user/deleteUserController.js
@@ -1,9 +1,18 @@
-import { deleteUser } from "../../models/userModel.js";
+import { deleteUser, validateUser } from "../../models/userModel.js";
 
 export async function deleteUsersController(req, res){
     const id = req.params.id
 
-    const result = await deleteUser(+id) // operador "+" para converter string para number
+    const {success, error, data} = validateUser({id: +id}, {name: true, avatar: true, pass: true, email: true})
+
+    if(!success){
+        return res.status(400).json({
+            message: "Erro de validação",
+            fieldErrors: error.flatten().fieldErrors
+        })
+    }
+
+    const result = await deleteUser(data.id) 
 
     return res.json({
         message: "Usuário deletado com sucesso!",
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -9,11 +9,19 @@ import * as z from 'zod'
 // }
 
 const userSchema = z.object({
-    id: z.int().positive(),
-    avatar: z.string().url().max(500),
-    name: z.string().min(3).max(255),
-    email: z.string().email(),
-    pass: z.string().min(6).max(255)
+    id: z.int("Id é obrigatório e deve ser um valor numérico")
+      .positive("Id deve ser um valor numérico positivo"),
+    avatar: z.string("Avatar é obrigatório e deve ser uma string")
+      .url("Avatar deve ser uma URL válida")
+      .max(500, "Avatar deve ter no máximo 500 caracteres"),
+    name: z.string("Nome deve ser uma string")
+      .min(3, "Nome deve ter no mínimo 3 caracteres")
+      .max(255, "Nome deve ter no máximo 255 caracteres"),
+    email: z.string("Email é obrigatório e deve ser uma string")
+      .email("Email deve ser um endereço de email válido"),
+    pass: z.string("Senha é obrigatória e deve ser uma string")
+      .min(6, "Senha deve ter no mínimo 6 caracteres")
+      .max(255, "Senha deve ter no máximo 255 caracteres")
 })
 
 export const validateUser = (user, partial = false) => {
