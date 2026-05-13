008cf87c2cb2a930a5836a9ac61f57809dc39db7
Renan Cavichi <renancavichi@gmail.com>
2026-05-12 21:28:12 -0300

    refactor: remove unnecessary blank lines in updateUsersController function

--- src/controllers/user/updateUserController.js
+++ src/controllers/user/updateUserController.js
@@ -7,7 +7,6 @@ export async function updateUsersController(req, res, next){
         user.id = +id
 
         const {success, error, data: userValidated} = validateUser(user)
-
         if(!success){
             return res.status(400).json({
                 message: "Erro de validação",
@@ -16,7 +15,6 @@ export async function updateUsersController(req, res, next){
         }
 
         const result = await updateUser(userValidated, userValidated.id)
-
         return res.json({
             message: "Usuário atualizado com sucesso!",
             user: result
