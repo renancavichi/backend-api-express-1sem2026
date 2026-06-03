2a7266df684eecebd94f25975dbcc85f43fac349
Renan Cavichi <renancavichi@gmail.com>
2026-06-02 21:50:42 -0300

    refactor: remove unnecessary console log from closeSessionsController

--- src/controllers/auth/closeSessions.js
+++ src/controllers/auth/closeSessions.js
@@ -4,8 +4,7 @@ export const closeSessionsController = async (req, res, next) => {
     const userId = req.userId
 
     try {
-        const result = await deleteAllSessionsByUserId(userId)
-        console.log(result)
+        await deleteAllSessionsByUserId(userId)
         res.clearCookie('refreshToken', { httpOnly: true, sameSite: 'None', secure: true })
         return res.status(200).json({
             message: "Todas as sessões foram encerradas com sucesso. Faça o login novamente!"
